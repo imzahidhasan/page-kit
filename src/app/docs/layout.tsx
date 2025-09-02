9b0239b777399e31404d84620144e48236503149
Zahid Hasan <imzahidhasan.mail@gmail.com>
2025-09-02 14:13:34 +0700

    Update installation guide and improve documentation formatting

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -9,7 +9,7 @@ export default function DocsLayout({ children }: { children: ReactNode }) {
       <Header />
       <div className="mx-auto max-w-[1536px] text-white flex flex-1 md:space-x-16">
         <Sidebar />
-        <main className="flex-1 py-6  pb-16 pt-8 min-w-0 max-w-4xl px-4">
+        <main className="flex-1 py-6 pb-16 pt-8 min-w-0 max-w-4xl px-4">
           <div className="prose prose-zinc dark:prose-invert prose-h1:text-3xl prose-h1:font-semibold prose-h2:text-xl prose-h2:font-semibold prose-h2:mt-14 prose-h3:text-lg prose-h3:font-medium prose-h3:scroll-m-20 max-w-full">
             {children}
           </div>
