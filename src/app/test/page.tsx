b42864b87ff902e37a28796daa4bc15684a58109
Zahid Hasan <imzahidhasan.mail@gmail.com>
2025-11-02 11:31:15 +0600

    feat: add LoginPage component with animations and responsive design

---
+++ src/app/test/page.tsx
@@ -0,0 +1,12 @@
+import LoginPage from '@/registry/blocks/login-and-signup-1/page'
+import React from 'react'
+
+const page = () => {
+  return (
+      <div>
+          <LoginPage />
+    </div>
+  )
+}
+
+export default page
